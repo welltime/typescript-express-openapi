90676844a4da3c37903fd5d9559e797b30d96757
Zauats <cfif12342002@yandex.ru>
2021-08-18 14:18:08 +0300

    add topic's items

--- src/log.ts
+++ src/log.ts
@@ -3,11 +3,45 @@ import chalk from 'chalk';
 
 export enum Topic {
     Unknown = 'unknown',
-    paymentProcessed = 'payment processed'
+    paymentProcessed = 'payment processed',
+
+    Init = 'init',
+    Api = 'api',
+    Adapter = 'adapter',
+    Integration = 'integration',
+    Config = 'config',
+    Message = 'message',
+    MessageCount = 'message-count',
+    Rabbitmq = 'rabbitmq',
+    Handler = 'handler',
+    Timer = 'timer',
 }
 
 export enum SubTopic {
     Unknown = 'unknown',
+
+    Rabbitmq = 'rabbitmq',
+    Rest = 'rest',
+    ScenarioTranslation = 'scenariotranslation',
+    Telegram = 'telegram',
+    Vk = 'vk',
+    Viber = 'viber',
+    Multichat = 'multichat',
+    Jivo = 'jivo',
+    Google = 'google',
+    Common = 'common',
+    McnWidget = 'mcn-widget',
+    InfiniteLoopBan = 'infiniteloopban',
+    Noinit = 'noinit',
+
+    OldMessagesFix = 'OldMessagesFix',
+    AnalyticsFix = 'AnalyticsFix',
+    LongTimerEvent = 'LongTimerEvent',
+
+    StatCall = 'StatCall',
+    User = 'user',
+    Client = 'client',
+    Role = 'role'
 }
 
 export enum Severity {
